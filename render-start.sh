4091c72a676481fc29b5d7764f4c816eac14011a
Rahul Maurya <maurya.rahul682@gmail.com>
2026-03-17 22:52:14 +0530

    update render-start.sh file to correct name

--- render-start.sh
+++ render-start.sh
@@ -7,4 +7,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-gunicorn Cricmind.wsgi:application --bind 0.0.0.0:10000
+gunicorn config.wsgi:application --bind 0.0.0.0:10000
